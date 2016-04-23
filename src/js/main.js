70af30aa1ea8026299685f06c83465c7b3de32c3
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-04-23 14:46:36 +0300

    Added store subscribe to rerender

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
+// copy index.html to public folder
 var index = require('../index.html'); // eslint-disable-line no-unused-vars
 
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App.jsx';
+import store from './stores/index.js';
 
-ReactDOM.render(
-	App,
-	document.getElementById('root')
-);
+
+const render = () => {
+	ReactDOM.render(
+		<App state={store.getState()} />,
+		document.getElementById('root')
+	);
+};
+
+store.subscribe(render);
+render();
